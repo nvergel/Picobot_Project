b1cdfd896b578a61b95bc1ec1bc01e35b4ef312f
Nicolas Vergel <nicolasvergel@Nicolass-MacBook-Pro-2.local>
2018-12-05 17:39:35 -0800

    wow

--- start.py
+++ start.py
@@ -18,63 +18,63 @@ NUMSTATES = 5
 # Program class:
 
 class Program:
-    """A class that represents a single Picobot program
-    """
-    def __init__(self):
-        """Construct objects of type Program, setting self.rules to be an empty dictionary
-        """
-        self.rules = {}
+	"""A class that represents a single Picobot program
+	"""
+	def __init__(self):
+		"""Construct objects of type Program, setting self.rules to be an empty dictionary
+		"""
+		self.rules = {}
 
-    def __repr__(self):
-        """This method returns a string representation of the Picobot program in such a way that it can
-           be pasted straight into the Picobot simulator
-        """
-        s = ""
+	def __repr__(self):
+		"""This method returns a string representation of the Picobot program in such a way that it can
+		   be pasted straight into the Picobot simulator
+		"""
+		s = ""
 
-        Keys = list(self.rules.keys()) 
-        sortedKeys = sorted(Keys)
+		Keys = list(self.rules.keys()) 
+		sortedKeys = sorted(Keys)
 
-        for key in sortedKeys:
-            s += str(key[0])+ " " + key[1] + " -> " + self.rules[key][0] + " " + str(self.rules[key][1]) + "\n"
+		for key in sortedKeys:
+			s += str(key[0])+ " " + key[1] + " -> " + self.rules[key][0] + " " + str(self.rules[key][1]) + "\n"
 
-        return s
+		return s
 
-    def randomize(self):
-        """This method generates a random full set of Picobot rules for the program's self.rules dictionary,
-           The set of rules is a combination of each Picobot state with all of the 9 possible surroundings
-        """
-        PATTERN = ["xxxx", "Nxxx", "NExx", "NxWx", "xxxS", "xExS", "xxWS", "xExx", "xxWx"]
-        POSSIBLE_MOVES = ["N", "S", "E", "W"]
+	def randomize(self):
+		"""This method generates a random full set of Picobot rules for the program's self.rules dictionary,
+		   The set of rules is a combination of each Picobot state with all of the 9 possible surroundings
+		"""
+		PATTERN = ["xxxx", "Nxxx", "NExx", "NxWx", "xxxS", "xExS", "xxWS", "xExx", "xxWx"]
+		POSSIBLE_MOVES = ["N", "S", "E", "W"]
 
-        for x in range(NUMSTATES):
-            for y in PATTERN:
-                movedir = random.choice(POSSIBLE_MOVES)
-                while movedir in y:
-                    movedir = random.choice(POSSIBLE_MOVES)
-                newstate = random.choice(range(NUMSTATES))
-                self.rules[(x, y)] = (movedir, newstate)
+		for x in range(NUMSTATES):
+			for y in PATTERN:
+				movedir = random.choice(POSSIBLE_MOVES)
+				while movedir in y:
+					movedir = random.choice(POSSIBLE_MOVES)
+				newstate = random.choice(range(NUMSTATES))
+				self.rules[(x, y)] = (movedir, newstate)
 
 	def getMove(self, state, surroundings):
 		return self.rules[(state, surroundings)]
 
 	def mutate(self):
-		state = random.choice(self.rules.keys())
+		state = random.choice(list(self.rules.keys()))
 		newdir = random.choice(POSSIBLE_MOVES)
-        while newdir in state[1]:
-            newdir = random.choice(range(NUMSTATES))
+		while newdir in state[1]:
+			newdir = random.choice(range(NUMSTATES))
 		self.rules[state] = (newdir, random.choice(range(NUMSTATES)))
 
-    
+	
 
 # World class:
-        
+		
 class World:
-    """
-    """
-    def __init__(self):
-        """
-        """
+	"""
+	"""
+	def __init__(self):
+		"""
+		"""
 
-    def __repr__(self):
-        """
-        """
+	def __repr__(self):
+		"""
+		"""
